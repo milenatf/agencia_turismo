8d990c0ad57077d808fac91a848148987261db83
milena_20 <milena.tavares@maiatecnologia.com.br>
2024-01-29 12:38:18 -0300

    Foi adicionado o auth breeze

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,36 @@
 
 use App\Http\Controllers\Panel\PanelController;
 use App\Http\Controllers\Site\SiteController;
+
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+// Route::get('/login', [])
 
 /** Rotas panel */
 Route::get('/panel', [PanelController::class, 'index'])->name('panel.index');
@@ -10,3 +39,5 @@ Route::get('/panel', [PanelController::class, 'index'])->name('panel.index');
 /** Rotas site */
 Route::get('/promocoes', [SiteController::class, 'promotions'])->name('promotions');
 Route::get('/', [SiteController::class, 'index'])->name('site.index');
+
+require __DIR__.'/auth.php';
