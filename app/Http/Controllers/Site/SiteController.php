eb544a72c23fe7160db111f0395e3c25d76dc909
milena_20 <milena.tavares@maiatecnologia.com.br>
2024-01-29 07:28:28 -0300

    Integração dos layouts

---
+++ app/Http/Controllers/Site/SiteController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use App\Http\Controllers\Controller;
+
+class SiteController extends Controller
+{
+    public function index()
+    {
+        $title = 'Home Page';
+
+        return view('site.home.index', compact('title'));
+    }
+
+    public function promotions()
+    {
+        $title = 'Promoções';
+
+        return view('site.promotions.list', compact('title'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\Panel\PanelController;
+use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
+/** Rotas panel */
 Route::get('/panel', [PanelController::class, 'index'])->name('panel.index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/** Rotas site */
+Route::get('/promocoes', [SiteController::class, 'promotions'])->name('promotions');
+Route::get('/', [SiteController::class, 'index'])->name('site.index');
